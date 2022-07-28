6aadf48091da29533bbacf71d22637e4a74aae98
unknown <341096105@gapps.yrdsb.ca>
2022-07-28 17:40:12 -0400

    Change Table and Event models

--- outline/models.py
+++ outline/models.py
@@ -4,30 +4,26 @@ from datetime import datetime
 
 # Create your models here.
 
-def create_database(connection):
-    cursor = connection.cursor()
-    cursor.execute(
-        "CREATE DATABASE events"
-    )
+class Table(models.Model):
+    name = models.TextField()
 
-def make_query(connection, query):
-    cursor = connection.cursor()
-    cursor.execute(query)
+    def __str__(self):
+        return self.name 
+    
 
-# add button to use previous startyear, month, day as endyear, month, day
 class Event(models.Model):
-    event_title = models.TextField()
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    text = models.TextField()
     start_date = models.DateField(default=datetime.today().strftime('%Y-%m-%d'))
-    start_time = models.TimeField(default="12:00")
+    start_time = models.TimeField()
+    end_date = models.DateField()
+    end_time = models.TimeField()
+    completed = models.BooleanField()
 
-def create_table(table_name):
-    with connection.cursor() as cursor:
-        query = "CREATE TABLE " + table_name + " ("
-        query += """ id INT PRIMARY KEY AUTOINCREMENT,
-                event_title TEXT DEFAULT NULL,
-                start_date DATE DEFAULT """ + str(datetime.today().strftime('%Y-%m-%d')) + ","
-        query += "start_time TIME DEFAULT 12:00)"
-        cursor.execute(query)
+    def __str__(self):
+        return self.text
+
+
 
 
 
--- outline/views.py
+++ outline/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from django.template import loader
 from django.db import connection
 from datetime import datetime
-from .models import Events
 from .models import create_table
 import calendar 
 
@@ -11,10 +10,10 @@ import calendar
 
 def index(request):
     cursor = connection.cursor()
-    all_tables = cursor.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'")
-    content = {}
-    for table in all_tables:
-        content[str(table)] = cursor.execute("SELECT * FROM "+str(table))
+    all_tables = cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    content = {'all_tables': all_tables}
+    # for table in all_tables:
+    #     content[str(table)] = cursor.execute("SELECT * FROM "+str(table))
     return render(request, 'index.html')
 
 def add_event(request):
@@ -34,23 +33,20 @@ def add_event_to_record(request):
         end_time = request.POST.get('end_time')
         cursor = connection.cursor()
         cursor.execute(
-            "INSERT INTO "+table+" VALUES ("+event_title+","+start_date+","+start_time+","+end_date+","+end_time
+            "INSERT INTO "+table+" VALUES ("+event_title+","+start_date+","+start_time+","+end_date+","+end_time+")"
         )
     
 
 def sort_by_date(request):
-    content = {
-        "all_events": Events.objects.raw("SELECT * FROM Events ORDER BY start_date ASC")
-    }
     return render(request, 'index.html', content)
 
 def filter_title(request):
     filter = request.POST('filter_title')
 
-def add_table(request):
+def add_table_to_record(request):
     if request.method == 'POST':
         table_name = str(request.POST.get('table_name'))
         cursor = connection.cursor()
         cursor.execute(
-            "CREATE TABLE " + table_name + " ("
+            "CREATE TABLE " + table_name + "id "
         )
